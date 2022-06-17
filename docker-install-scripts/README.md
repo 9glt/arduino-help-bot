9c63632d40fa7320a6876d0e99023025683c4352
Mindaugas Žvirblis <mz@9g.lt>
2022-06-17 14:00:07 +0300

    docker install scripts

---
+++ docker-install-scripts/README.md
@@ -0,0 +1,7 @@
+# Docker installation sscripts
+
+If you dont have docker in your distro, run one of scripts depending on distro
+
+```
+bash docker-install-ubuntu.sh
+```
---
+++ docker-install-scripts/docker-install-ubuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+apt-get -y remove docker docker-engine docker.io containerd runc
+
+apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+
+install_docker_compose () {
+        curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
+        shasum -a 256 -s -c docker-compose-linux-x86_64.sha256 && mv ./docker-compose-linux-x86_64 /usr/local/sbin/docker-compose && chmod +x /usr/local/sbin/docker-compose && rm -f docker-compose-linux-x86_64.sha256 && return 0
+        echo "problem installing docker compose"
+        exit 666
+}
+
+install_docker_compose
